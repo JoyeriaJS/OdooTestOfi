86b5ef3245b9f9c250a604f84898af91343d828e
unknown <dante.h@hotmail.es>
2026-03-19 14:31:55 -0300

    cambiosdesglosepos100

--- custom_addons/joyeria_reparaciones/static/src/js/pos_special_products.js
+++ custom_addons/joyeria_reparaciones/static/src/js/pos_special_products.js
@@ -150,13 +150,15 @@ patch(Order.prototype, {
             const line = this.get_selected_orderline();
 
             // ✅ FIX REACTIVIDAD OWL (CLAVE)
-            line.set({
-                numero_rma: resultado.rma,
-                precio_original_rma: precio_backend,
-                subtotal_rma: resultado.subtotal,
-                abono_rma: resultado.abono,
-                saldo_rma: resultado.saldo,
-            });
+            line.numero_rma = resultado.rma;
+            line.precio_original_rma = precio_backend;
+            line.subtotal_rma = resultado.subtotal;
+            line.abono_rma = resultado.abono;
+            line.saldo_rma = resultado.saldo;
+
+            // 🔥 FORZAR REACTIVIDAD
+            line.trigger('change');
+            
 
             return;
         }
